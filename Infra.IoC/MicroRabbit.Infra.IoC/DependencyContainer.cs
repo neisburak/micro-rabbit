f295943e2a427ff914847babaa0ec8a18221e616
Burak Neiş <burakneis@Buraks-MacBook-Pro.local>
2022-11-01 17:02:45 +0300

    initial commit of banking microservice

--- Infra.IoC/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ Infra.IoC/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,3 +1,8 @@
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,6 +13,14 @@ public class DependencyContainer
 {
     public static void RegisterServices(IServiceCollection services)
     {
+        // Domain Bus
         services.AddTransient<IEventBus, RabbitMQBus>();
+
+        // Application Services
+        services.AddTransient<IAccountService, AccountService>();
+
+        // Data
+        services.AddTransient<IAccountRepository, AccountRepository>();
+        services.AddTransient<BankingDbContext>();
     }
 }
